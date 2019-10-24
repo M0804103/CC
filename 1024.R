591af22da8158177b0436f1f39a22ed50bfead20
cc <m0804103@im.tpcu.edu.tw>
2019-10-24 21:31:39 +0800

    1024

---
+++ 1024.R
@@ -0,0 +1,50 @@
+x=100:110
+
+
+x<-1:6
+x+3
+x + 6:7
+
+seq(1,20,0.3)
+1:9
+seq(0,100,length.out=5)
+x=1:6
+x=c(1,2,3,4,5,6)
+x=c(1,3,7,2,9)
+baskets.NBA2016.Lin = c(7,8,6,11,9,12)
+score.NBA2016.Lin = baskets.NBA2016.Lin * 2
+score.NBA2016.Lin
+baskets.NBA2016.Jorden = c(10,5,9,12,7,11)
+(baskets.NBA2016.Lin+baskets.NBA2016.Jorden) * 2
+
+rep(1:5,each=2,length.out=8)
+y=c(0,0,0,0,0)
+y1=rep(0,5)
+y2=numeric(5)
+a=5
+class(a)
+s = "hello"
+class(s)
+
+y3=(baskets.NBA2016.Lin+baskets.NBA2016.Jorden) * 2 
+y4=rep(1:5,time = 
+         2
+       ,each=2)
+s = "hello
+     world"
+s
+baskets.NBA2016.Lin = c (7,8,6,11,9,12)
+sum(baskets.NBA2016.Lin)
+
+x=sample(1:100,10)
+x
+sort(x)
+rank(-x)
+length(x)
+sd(x)^2
+var(x)
+x="hello"
+length(x)
+nchar(y)
+x1=c("h","e","l","l","o")
+length(x1)
